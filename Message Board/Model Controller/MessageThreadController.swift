0c61bbe29c3c566d7b10967d0eaf64d471ddeee4
William <chen.will7@gmail.com>
2019-10-11 14:25:12 -0700

    fixed the decoding

--- Message Board/Model Controller/MessageThreadController.swift
+++ Message Board/Model Controller/MessageThreadController.swift
@@ -31,11 +31,16 @@ class MessageThreadController {
             guard let data = data else { NSLog("No data returned from data task"); completion(); return }
             
             do {
-                self.messageThreads = try JSONDecoder().decode([String: MessageThread].self, from: data)
+//                self.messageThreads = Array(try JSONDecoder().decode([String: MessageThread].self, from: data))
+                
+                self.messageThreads = Array(try JSONDecoder().decode([String: MessageThread].self, from: data).values)
+                
             } catch {
                 self.messageThreads = []
                 NSLog("Error decoding message threads from JSON data: \(error)")
             }
+            
+            
             
             completion()
         }.resume()
